61ad71d6ea985d85bc7768a2d681a48256465483
Rosell22V <Gustav@klitholm.com>
2023-02-22 17:44:03 +0100

    Fixede småting

--- 4_modul/opgEx/app.js
+++ 4_modul/opgEx/app.js
@@ -5,32 +5,32 @@ const readline = require('readline').createInterface({
 })
 
 // Noget JavaScript der læser et svar på et spørgsmål fra terminalen.
-readline.question(`Hvad bliver næste terningekast? `, function (gæt1) {
-    console.log(`Spiller 1 gætter på ${gæt1}!`);
+readline.question(`\nSpiller 1 starter!\nGæt: `, function (gæt1) {
+    console.log(`\nSpiller 1 gætter på ${gæt1}! `);
 
-    readline.question(`Hvad bliver næste terningekast? `, function (gæt2) {
-        console.log(`Spiller 2 gætter på ${gæt2}!`);
+    readline.question(`\nHvad gætter spiller 2 på mon på?\nGæt: `, function (gæt2) {
+        console.log(`\nSpiller 2 gætter på ${gæt2}!`);
 
         // Kast terning: 
         let terning = Math.floor(Math.random() * 6 + 1); // Antal sider
-        console.log(`Der blev kastet ${terning}!`);
+        console.log(`\nEr vi ude i en siuu eller SIUU...\n....det bliveeer....\n--------------------\n         ${terning}!\n--------------------`);
 
         // TODO: Udskriv om spilleren gættede rigtigt eller forkert!
         if (gæt1 == terning && gæt2 == terning) { // Uafgjort
-            console.log("I gættede begge rigtigt! Uafgjort, siuuu...");
+            console.log("\nI gættede begge rigtigt! Uafgjort, siuuu...\n");
         } else if (gæt1 == terning) { // Spiller 1 vinder
-            console.log("Spiller 1 gættede rigtigt! SIUUUUU");
+            console.log("\nSpiller 1 gættede rigtigt! SIUUUUU\n");
         } else if (gæt2 == terning) { // Spiller 2 vinder
-            console.log("Spiller 2 gættede rigtigt! SIUUUUU");
+            console.log("\nSpiller 2 gættede rigtigt! SIUUUUU\n");
         } else if (gæt1 != terning && gæt2 != terning) { // Spiller som er tættest på vinder
             let diff1 = Math.abs(terning - gæt1);
             let diff2 = Math.abs(terning - gæt2);
             if (diff1 < diff2) {
-                console.log("Spiller 1 var tættest på! SIUUUUU");
+                console.log("\nSpiller 1 var tættest på! SIUUUUU\n");
             } else if (diff2 < diff1) {
-                console.log("Spiller 2 var tættest på! SIUUUUU");
+                console.log("\nSpiller 2 var tættest på! SIUUUUU\n");
             } else {
-                console.log("I var begge lige tæt på! Uafgjort, siuuu...");
+                console.log("\nI var begge lige tæt på! Uafgjort, siuuu...\n");
             }
             readline.close();
         }
