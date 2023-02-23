cfd1d14fc26cb9509a2b61ffaa95de95fbe7fc3d
Rosell22V <Gustav@klitholm.com>
2023-02-23 12:50:32 +0100

    Update; gjort diverse opgaver pænere

--- 5_modul/opg8/app.js
+++ 5_modul/opg8/app.js
@@ -6,13 +6,16 @@ For hver funktion skal du huske at teste den med forskellige input, og printe ou
 
 Lav følgende funktioner:
 
-1. En funktion der tager et array med tekst som input. 
+8.1: En funktion der tager et array med tekst som input. 
 Funktionen skal returnerer summen af længderne på alle tekster i arrayet.
-2. En funktion der tager to arrays, og returnerer et nyt langt array hvor elementer fra de to arrays er sat sammen. 
+
+8.2: En funktion der tager to arrays, og returnerer et nyt langt array hvor elementer fra de to arrays er sat sammen. 
 Fx bliver [1, 2, 3] og [4, 5, 6] til [1, 2, 3, 4, 5, 6].
-3. En funktion der tager to arrays med samme længde, og hvor begge indeholder tal. 
+
+8.3: En funktion der tager to arrays med samme længde, og hvor begge indeholder tal. 
 Funktionen skal returnerer et nyt array hvor tallene er lagt parvis sammen. Fx hvis du har [1, 2, 3] + [4, 5, 6], så giver det [5, 7, 9] når de er lagt sammen.
-4. En funktion der tager et array med tekst som input. 
+
+8.4: En funktion der tager et array med tekst som input. 
 Funktionen skal returnere den længste tekst i arrayet. */
 
 // Opgave 8.1:) En funktion der tager et array med tekst som input. Funktionen skal returnerer summen af længderne på alle tekster i arrayet.
@@ -52,11 +55,12 @@ console.log("-------------------------------------------------------------------
 console.log("Opgave 8.2: Funtkion, som tager 2 arrays og returnerer et nyt med elementerne fra begge arrays:")
 console.log("----------------------------------------------------------------------------------------------------------------------------\n")
 
-console.log("Sammenlagt nyt array af arr1 + arr2: \n" + combineArrays(["Jørgen, GMoney, Oskar "], ["Helle, Sofus, Selma"]) + "\n"); // Printer
+console.log("Sammenlagt nyt array af arr1 + arr2: \n" + combineArrays(["Jørgen, GMoney, Oskar "], ["Helle, Sofus, Selma"]) + "\n"); // Printer 'Jørgen, GMoney, Oskar Helle, Sofus, Selma'
 
 console.log("----------------------------------------------------------------------------------------------------------------------------\n")
 
-// Opgave 8.3:) En funktion der tager to arrays med samme længde, og hvor begge indeholder tal. Funktionen skal returnerer et nyt array hvor tallene er lagt parvis sammen.
+// Opgave 8.3:) En funktion der tager to arrays med samme længde, og hvor begge indeholder tal. 
+// Funktionen skal returnerer et nyt array hvor tallene er lagt parvis sammen.
 
 function combineArrSum(array1, array2) {
     let newArray = [];
@@ -71,7 +75,7 @@ console.log("-------------------------------------------------------------------
 console.log("Opgave 8.3: Funktion, som tager 2 arrays og returnerer et nyt med summen af elementerne fra begge arrays:")
 console.log("----------------------------------------------------------------------------------------------------------------------------\n")
 
-console.log("Sammenlagt nyt array af arr1 + arr2: \n" + combineArrSum([1, 2, 3], [4, 5, 6])); // Printer
+console.log("Sammenlagt nyt array af arr1 + arr2: \n" + combineArrSum([1, 2, 3], [4, 5, 6])); // Printer [5, 7, 9]
 
 console.log("----------------------------------------------------------------------------------------------------------------------------\n")
 
