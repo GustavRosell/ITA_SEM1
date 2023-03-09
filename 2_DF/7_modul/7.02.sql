1b776969274e5e6f08fc46b69e4d950ca2bdb02e
Rosell22V <Gustav@klitholm.com>
2023-03-09 19:50:08 +0100

    Update; Færdig med opg 7.02

--- 2_DF/7_modul/7.02.sql
+++ 2_DF/7_modul/7.02.sql
@@ -1,8 +1,8 @@
--- Opgave 1 -- 
+-- Opgave A -- 
 
 /*Create Database IBAS;*/
 
--- Opgave 2 -- 
+-- Opgave B -- 
 
 CREATE TABLE bicycle (
 bicycle_id SERIAL Primary Key,
@@ -18,12 +18,12 @@ Add "bicycle_type" varchar(45);
 Alter Table bicycle
 Drop bicycle_name;
 
--- Opgave 3 --
+-- Opgave C --
 
 Insert into bicycle (bicycle_type) 
 Values('Racer'), ('Mountain'), ('BMX'), ('Dirt'), ('City');
 
--- Opgave 4 --
+-- Opgave D --
 
 Create table production ( 
 bicycle_id SERIAL Primary Key,
@@ -36,8 +36,8 @@ Constraint fk_type_id FOREIGN KEY (bike_type_id)
 	References bicycle(bicycle_id)
 );
 
--- Opgave 5 --
-/* a)
+
+/* E.1)
 Indsætter 3 cykler 
 */
 
@@ -50,12 +50,11 @@ Values ('2023-03-09 10:00:00', 2010, 5),
 ('2023-03-07 09:15:00', 6000, 7);
 
 
-/* b)
+/* E.2)
 
 Indsætter en cykel fra 2020 */
 Insert into production (production_dt, ral_color_code, bike_type_id) 
 Values ('2019-03-09 10:00:00', 8000, 5);
-
 
 /* 
 Hvilken regel er i spil her?
@@ -63,6 +62,70 @@ Svar:
 ERROR:  new row for relation "production" violates check constraint "production_production_dt_check"
 	CHECK (production_dt >= '2023-01-01 00:00:00'),
 */
-/* c)
 
+-- E.3) Indsæt en cykel med et bike_type_id som ikke findes i bicycle_type tabellen. -- 
+
+select bike_type_id
+From production;
+
+Insert into production (bike_type_id)
+Values(15);
+
+/*
+ERROR:  null value in column "production_dt" of relation "production" violates not-null constraint
+DETAIL:  Failing row contains (8, null, null, 15).
 */
+
+-- E.4) Indsæt en cykel med farvekode mindre end 1000 og en cykel med farvekode større end 9999. -- 
+
+/* gider ikke til at prøve det her igen, det giver jo sig selv; 
+Det er ikke muligt at indsætte cykler med farvekode mindre end 1000 eller større end 9999 i tabellen, 
+da der er en CHECK-begrænsning på ral_color_code kolonnen, 
+der sikrer, at værdien skal være mellem 1000 og 9999.
+*/
+
+-- Opgave F) Opret en customer tabel. 
+/* 
+Tilføj kolonner for: customer_id, name, address, postalcode, city og country. 
+Overvej: datatyper, nøgler og constraints. Lidt hjælp: Customer id skal være primary key. 
+Overvej om der er nogle constraints på postal_code – vi antager danske postnumre.
+*/
+
+CREATE TABLE customer (
+  customer_id SERIAL PRIMARY KEY,
+  name varchar(50) NOT NULL,
+  address varchar(50) NOT NULL,
+  postalcode CHAR(4) NOT NULL,
+  city varchar(50) NOT NULL,
+  country varchar(50) NOT NULL,
+	CONSTRAINT postalcode_format CHECK (LENGTH(postalcode) = 4)
+);
+
+select * 
+from customer;
+
+-- Opgave G) 
+/*
+Opret en tabel med navnet shipment. 
+Tilføj felterne: shipment_id, bicycle_id, shipment_date, customer_id, carrier_name.
+Overvej: datatyper, nøgler og constraints og hvordan sammenhængen med customer tabellen fra opgave F og tabellen fra D.
+Lidt hjælp:
+shipment_id skal være Primary Key.
+Customer_id skal være foreign_key som refererer til customer(customer_id) (altså til customer_id I customer tabellen)
+Bicycle_id skal være en foreign key som referer til bicycle_production(bicycle_id)
+*/
+
+CREATE TABLE shipment (
+	shipment_id SERIAL PRIMARY KEY,
+	bicycle_id INTEGER NOT NULL,
+	shipment_date DATE NOT NULL,
+	customer_id INTEGER NOT NULL,
+	carrier_name VARCHAR(50) NOT NULL,
+	CONSTRAINT fk_customer_id FOREIGN KEY (customer_id) 
+		REFERENCES customer(customer_id),
+	CONSTRAINT fk_bicycle_id FOREIGN KEY (bicycle_id) 
+		REFERENCES production(bicycle_id)
+);
+
+Select *
+from shipment;
