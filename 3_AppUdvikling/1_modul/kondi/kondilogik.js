8b39d90c56e969074fb5e36ace473b21fef3b409
rosell <Gustavrosell@gmail.com>
2023-08-30 15:06:12 +0200

    opg 6 færdig

--- 3_AppUdvikling/1_modul/kondi/kondilogik.js
+++ 3_AppUdvikling/1_modul/kondi/kondilogik.js
@@ -2,3 +2,18 @@ function BeregnKondiTal(dist, isMale) {
     var kondi = 18.38 + (0.03301 * dist) - (5.92 * (isMale ? 0 : 1));
     return kondi;
 }
+
+function VurderKondiTalNiveau(kondital, alder) {
+    // Implementer vurdering af konditalniveau baseret på alder og kondital-værdi
+    if (alder >= 0 && alder <= 100) {
+        if (kondital < 28) {
+            return "Lav";
+        } else if (kondital < 50) {
+            return "Middel";
+        } else {
+            return "Høj";
+        }
+    } else {
+        return "Ugyldig alder";
+    }
+}
